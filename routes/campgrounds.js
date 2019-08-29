c206246ddd861ecf040359563128764dfe0aa51f
Amrit Khera <amritkhera98@gmail.com>
2019-08-29 12:43:39 +0530

    YelpCamp v9

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -15,12 +15,15 @@ router.get("/",function(req,res){
 });
 
 //CREATE - add new campground to DB
-router.post("/",function(req,res){
+router.post("/",isLoggedIn,function(req,res){
 	var name=req.body.name;
 	var image=req.body.image;
 	var desc=req.body.description;
-
-	var newCampground={name:name,image:image,description:desc};
+	var author={
+		id:req.user._id,
+		username:req.user.username
+	};
+	var newCampground={name:name, image:image, description:desc, author:author};
 	Campground.create(newCampground,function(err,newlyCreated){
 		if(err){
 			console.log(err);
@@ -32,7 +35,7 @@ router.post("/",function(req,res){
 });
 
 //NEW-Show form to create new campground
-router.get("/new",function(req,res){
+router.get("/new",isLoggedIn,function(req,res){
 	res.render("campgrounds/new");
 });
 
@@ -47,5 +50,12 @@ router.get("/:id",function(req,res){
 		}
 	});
 });
+
+function isLoggedIn(req,res,next){
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect("/login");
+}
 
 module.exports=router;
