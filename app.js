a0822628a5f417556112d774ecac205e829eb93a
Amrit Khera <amritkhera98@gmail.com>
2019-08-31 14:25:52 +0530

    correct add remote db

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var commentRoutes			=	require("./routes/comments"),
 	indexRoutes				=	require("./routes/index");
 
 //mongoose.connect("mongodb://localhost:27017/yelp_camp",{useNewUrlParser:true});
-mongoose.connect("mongodb+srv://AmritK10:<ConnectOnline69>@yelpcamp-qrjnk.mongodb.net/test?retryWrites=true&w=majority",{useNewUrlParser:true});
+mongoose.connect("mongodb+srv://AmritK10:ConnectOnline69@yelpcamp-qrjnk.mongodb.net/test?retryWrites=true&w=majority",{useNewUrlParser:true});
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
