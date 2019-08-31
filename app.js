e0d299b14e9c98d6c3c768318699c3dba6a14544
Amrit Khera <amritkhera98@gmail.com>
2019-08-31 14:41:01 +0530

    add env variables

--- app.js
+++ app.js
@@ -16,8 +16,7 @@ var commentRoutes			=	require("./routes/comments"),
 	campgroundRoutes		=	require("./routes/campgrounds"),
 	indexRoutes				=	require("./routes/index");
 
-//mongoose.connect("mongodb://localhost:27017/yelp_camp",{useNewUrlParser:true});
-mongoose.connect("mongodb+srv://AmritK10:ConnectOnline69@yelpcamp-qrjnk.mongodb.net/test?retryWrites=true&w=majority",{useNewUrlParser:true});
+mongoose.connect(process.env.DATABASEURL,{useNewUrlParser:true});
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
@@ -50,6 +49,6 @@ app.use("/campgrounds",campgroundRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 
 // process.env.PORT,process.env.IP
-app.listen(process.env.PORT,process.env.IP,function(){
+app.listen(process.env.PORT || 3000,process.env.IP,function(){
 	console.log("YelpCamp server has started!!")
 });
